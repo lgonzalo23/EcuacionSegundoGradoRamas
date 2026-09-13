02cc65ae2916d35ab676e28bcbd1d6035f003711
Gonzalo Pumaccahua <gonzalopumaccahua@gmail.com>
2026-09-13 18:49:49 -0500

    Mejora 1: correccion del signo de la formula

--- src/ecuacionsegundogradoramas/EcuacionSegundoGradoRamas.java
+++ src/ecuacionsegundogradoramas/EcuacionSegundoGradoRamas.java
@@ -17,16 +17,22 @@ public class EcuacionSegundoGradoRamas {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int a, b, c;
         double x1, x2;
+
         System.out.print("Ingrese a: ");
         a = sc.nextInt();
+
         System.out.print("Ingrese b: ");
         b = sc.nextInt();
+
         System.out.print("Ingrese c: ");
         c = sc.nextInt();
-        x1 = (b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
-        x2 = (b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+
+        x1 = (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+        x2 = (-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
+
         System.out.println("Raiz 1: " + x1);
         System.out.println("Raiz 2: " + x2);
     }
